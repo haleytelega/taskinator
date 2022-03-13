828f00d4ba6d8b986e0b0630d4444de9dbf3d059
Haley Telega <haleytelega@outlook.com>
2022-03-13 16:26:09 -0400

    refactored the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,22 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do"); // task item DOM refernce
 
-var createTaskHandler = function (event) { //placing code into a seperate function to hold a single responsibility
+var taskFormHandler = function (event) { //placing code into a seperate function to hold a single responsibility
     event.preventDefault(); //prevents the browser from refreshing and carring out its default bahaviour
     var taskNameInput = document.querySelector("input[name='task-name']").value; //adding text to the list item - important when making dynmaic elements
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
-    
+
+    //package up data as an object
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+
+    //send it as an argument to createTaskEl
+    createTaskEl(taskDataObj);
+};
+
+var createTaskEl = function(taskDataObj){
     //create list iem
     var listItemEl = document.createElement("li"); // creating the DOM object element - important when making dynmaic elements
     listItemEl.className = "task-item"; // adding css styling using exisitng css - important when making dynmaic elements
@@ -14,12 +25,12 @@ var createTaskHandler = function (event) { //placing code into a seperate functi
     //create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
     taskInfoEl.className = "task-info";
-    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
     listItemEl.appendChild(taskInfoEl); //appending this elemnt to the task list - important when making dynmaic elements
 
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
-formEl.addEventListener("submit", createTaskHandler); //saying on a button click, create a task
+formEl.addEventListener("submit", taskFormHandler); //saying on a button click, create a task
 
