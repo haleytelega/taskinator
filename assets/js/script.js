b5e581518e0b6dd10f181bbf6ef1b87442de890e
Haley Telega <haleytelega@outlook.com>
2022-03-13 15:39:46 -0400

    added more js to accept different tasks names and options

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,21 @@ var tasksToDoEl = document.querySelector("#tasks-to-do"); // task item DOM refer
 
 var createTaskHandler = function (event) { //placing code into a seperate function to hold a single responsibility
     event.preventDefault(); //prevents the browser from refreshing and carring out its default bahaviour
+    var taskNameInput = document.querySelector("input[name='task-name']").value; //adding text to the list item - important when making dynmaic elements
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    //create list iem
     var listItemEl = document.createElement("li"); // creating the DOM object element - important when making dynmaic elements
     listItemEl.className = "task-item"; // adding css styling using exisitng css - important when making dynmaic elements
-    listItemEl.textContent = "This is a new task."; //adding text to the list item - important when making dynmaic elements
-    tasksToDoEl.appendChild(listItemEl); //appending this elemnt to the task list - important when making dynmaic elements
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl); //appending this elemnt to the task list - important when making dynmaic elements
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", createTaskHandler); //saying on a button click, create a task
